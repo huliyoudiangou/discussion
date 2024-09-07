49d8c18efdbecc65432edae50752a805d165d38a
huliyoudiangou <75266067+huliyoudiangou@users.noreply.github.com>
2024-09-07 14:09:55 +0800

    Update XFooter.vue

--- components/XFooter.vue
+++ components/XFooter.vue
@@ -9,15 +9,9 @@ withDefaults(defineProps<{
 <template>
   <div class="footer max-w-[1080px] mx-auto flex gap-2 text-xs my-4 px-2 md:px-0 pb-4 text-gray-400 justify-between md:justify-normal relative">
     <span class="">
-      @2024
     </span>
-    <span>版本v{{ version }}</span>
     <span class="mx-1">•</span>
-    <a target="_blank" href="https://m.mblog.club">极简朋友圈</a>
-    <span class="mx-1">•</span>
-    <a target="_blank" href="https://dev.mblog.club">极简博客</a>
-    <span class="mx-1">•</span>
-    <a target="_blank" href="https://discussion.mblog.club">极简论坛</a>
+    <a target="_blank" href="https://makifx.com">MAKI分享</a>
   </div>
 </template>
 
